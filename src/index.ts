8339aa5c36650693ba1f8c485e45698d5eaca517
Derrick Hammer <derrick@derrickhammer.com>
2022-07-29 23:39:28 -0400

    *prettier

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-export { kernelLoaded, loginComplete, logoutComplete, openAuthWindow } from "./auth.js"
-export { kernelVersion } from "./messageversion.js"
-export { callModule, connectModule, init, newKernelQuery } from "./queries.js"
-export { addContextToErr, checkObj } from "libskynet"
+export {
+  kernelLoaded,
+  loginComplete,
+  logoutComplete,
+  openAuthWindow,
+} from "./auth.js";
+export { kernelVersion } from "./messageversion.js";
+export { callModule, connectModule, init, newKernelQuery } from "./queries.js";
+export { addContextToErr, checkObj } from "libskynet";
